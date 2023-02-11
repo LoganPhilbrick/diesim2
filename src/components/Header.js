68c21be4a18146e8b3fa5097805bc069e4f777c9
Logan Philbrick <loganphilbrick99@gmail.com>
2023-02-11 15:03:48 -0500

    initial setup & added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+// import { Navbar, NavbarBrand, Row, Col, Container } from "reactstrap";
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-dark">
+      <a className="navbar-brand m-auto p-4" href="/">
+        Tabletop-Die
+      </a>
+    </nav>
+  );
+};
+
+export default Header;
