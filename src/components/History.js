c39ae5ebdeb3b4e77104315aad888735805ac006
Logan Philbrick <loganphilbrick99@gmail.com>
2023-03-11 00:56:03 -0500

    commit :)

--- src/components/History.js
+++ src/components/History.js
@@ -12,7 +12,7 @@ const HistoryBox = () => {
     <Col id="historyBox" xs="10" md="3" className="boxShadow blueBorder ms-md-5">
       <div style={{ display: "flex", justifyContent: "center", position: "relative" }} className="border-bottom">
         <h5 className="text-center p-3 ms-md-3 text-light">History</h5>
-        <button style={{ position: "absolute" }} id="reset" className="btn shadow-none text-light" onClick={() => dispatch(resetHistory)}>
+        <button style={{ position: "absolute" }} id="reset" className="btn shadow-none text-light" onClick={() => dispatch(resetHistory())}>
           <FontAwesomeIcon icon={faRedo} />
         </button>
       </div>
