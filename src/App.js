8c74e7dca6e465f36df0d28661b8832d29742091
Logan Philbrick <loganphilbrick99@gmail.com>
2023-03-15 15:43:42 -0400

    implimented Redux-persist

--- src/App.js
+++ src/App.js
@@ -8,24 +8,30 @@ import BottomButtonGroup from "./components/BottomButtonGroup";
 import RollButton from "./components/RollButton";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 
 function App() {
   return (
     <Provider store={store}>
-      <div>
-        <Header />
-        <Container fluid>
-          <Row className="pt-5 ps-0 pe-0 pb-4 ps-md-5 justify-content-center order-md-first">
-            <HistoryBox />
-            <Col className="order-first order-md-last">
-              <ResultsBar />
-              <TopButtonGroup />
-              <BottomButtonGroup />
-              <RollButton />
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <PersistGate persistor={persistor}>
+        <div>
+          <Header />
+          <Container fluid>
+            <Row className="pt-5 ps-0 pe-0 pb-4 ps-md-5 justify-content-center order-md-first">
+              <HistoryBox />
+              <Col className="order-first order-md-last">
+                <ResultsBar />
+                <TopButtonGroup />
+                <BottomButtonGroup />
+                <RollButton />
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import dieSelectionReducer from "./dieSelection";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  dieSelection: dieSelectionReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: {
-    dieSelection: dieSelectionReducer,
-  },
+  reducer: persistedReducer,
 });
